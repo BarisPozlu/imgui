4022fd31739c9d46ba7db2be86539d127e0b6a80
BarisPozlu <131907140+BarisPozlu@users.noreply.github.com>
2024-12-05 03:53:24 +0300

    Added premake file.

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+project "ImGui"
+
+	kind "StaticLib"
+	language "C++"
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+        "imgui.cpp",
+        "imgui.h",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imgui_rectpack.h",
+        "imgui_textedit.h",
+        "imgui_truetype.h"
+	}
+
+	includedirs
+	{
+		".",
+		"../GLFW/include"
+	}
+
+	filter "system:windows"
+
+		systemversion "latest"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+
+		runtime "Release"
+		optimize "on"
